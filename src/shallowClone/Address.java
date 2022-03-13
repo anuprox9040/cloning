2e094eb665a0869f4d9705c5c0133ce17276d93f
anuprox9040 <anuprox9040@gmail.com>
2022-03-13 13:50:03 +0530

    first commit

---
+++ src/shallowClone/Address.java
@@ -0,0 +1,24 @@
+package shallowClone;
+
+public class Address {
+	
+	String city;
+	String country;
+	int pin;
+	
+	public Address() {
+		super();
+	}
+
+	public Address(String city, String country, int pin) {
+		super();
+		this.city = city;
+		this.country = country;
+		this.pin = pin;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [city=" + city + ", country=" + country + ", pin=" + pin + "]";
+	}
+}
